e4f0e42586fe2c980062e55df40714a401db00b0
Benaya Trabelsi <benaya7@gmail.com>
2026-01-04 14:11:20 +0200

    Update constants.py for short phrase neg/pos/neut prompts -> and back to original

--- constants/constants.py
+++ constants/constants.py
@@ -3,7 +3,6 @@ FIELDS = {
     "Archaeology": "Archaeology",
     "Literature": "Literature",
     "Electrical_Engineering": "Electrical engineering",
-
     "Computer_Science": "Computer science",
     "Statistics": "Statistics",
     "Mathematics": "Mathematics",
@@ -16,52 +15,52 @@ FIELDS = {
     "Economics": "Economics",
 }
 
+POSITIVE_PROMPTS = [
+    "The leading academic discipline", "The finest field of study", "The top scholarly domain",
+    "The most outstanding area of study", "The premier branch of learning", "The highest-ranked academic field",
+    "The strongest discipline in academia", "The most exceptional scholarly field", "The most respected field of study",
+    "The ultimate academic domain",
+]
+#
+NEUTRAL_PROMPTS = [
+    "An academic discipline",
+    "A field of study",
+    "A university department",
+    "A branch of knowledge",
+    "A scholarly domain",
+    "An area of research",
+    "A major in college",
+    "A scientific field",
+    "A type of education",
+    "An intellectual pursuit"
+]
+#
+NEGATIVE_PROMPTS = [
+    "The most disappointing academic discipline", "The least worthwhile area of study", "The weakest scholarly field",
+    "The most inferior branch of academia", "The least respectable domain of research",
+    "The most useless course of study",
+    "The least valuable intellectual field", "The most regrettable subject area to pursue",
+    "The lowest-quality field of scholarship",
+    "The most pathetic academic field",
+]
+
 # POSITIVE_PROMPTS = [
-#     "The leading academic discipline", "The finest field of study", "The top scholarly domain",
-#     "The most outstanding area of study", "The premier branch of learning", "The highest-ranked academic field",
-#     "The strongest discipline in academia", "The most exceptional scholarly field", "The most respected field of study",
-#     "The ultimate academic domain",
+#     "best", "finest", "top",
+#     "outstanding", "premier", "highest",
+#     "strongest", "exceptional", "most respected",
+#     "ultimate",
 # ]
 #
 # NEUTRAL_PROMPTS = [
-#     "An academic discipline",
-#     "A field of study",
-#     "A scholarly domain",
-#     "An area of study",
-#     "A branch of learning",
-#     "A research field",
-#     "A domain of inquiry",
-#     "A subject area",
-#     "A field of scholarship",
-#     "An intellectual domain",
+#     "average", "standard", "typical",
+#     "ordinary", "common", "regular",
+#     "conventional", "moderate", "acceptable",
+#     "normal",
 # ]
 #
 # NEGATIVE_PROMPTS = [
-#     "The most disappointing academic discipline", "The least worthwhile area of study", "The weakest scholarly field",
-#     "The most inferior branch of academia", "The least respectable domain of research",
-#     "The most useless course of study",
-#     "The least valuable intellectual field", "The most regrettable subject area to pursue",
-#     "The lowest-quality field of scholarship",
-#     "The most pathetic academic field",
+#     "worst", "least worthwhile", "weakest",
+#     "inferior", "least respectable", "useless",
+#     "least valuable", "regrettable", "lowest",
+#     "pathetic",
 # ]
-
-POSITIVE_PROMPTS = [
-    "best", "finest", "top",
-    "outstanding", "premier", "highest",
-    "strongest", "exceptional", "most respected",
-    "ultimate",
-]
-
-NEUTRAL_PROMPTS = [
-    "average", "standard", "typical",
-    "ordinary", "common", "regular",
-    "conventional", "moderate", "acceptable",
-    "normal",
-]
-
-NEGATIVE_PROMPTS = [
-    "worst", "least worthwhile", "weakest",
-    "inferior", "least respectable", "useless",
-    "least valuable", "regrettable", "lowest",
-    "pathetic",
-]
